df6e0d57a398fe99e978db9917306264592bf95d
Jiahui Li <stanley@Jiahuis-MacBook-Air.local>
2023-01-22 16:24:08 -0500

    initial setup and some practice

---
+++ src/main/scala/lectures/AdvancedPatternMatching.scala
@@ -0,0 +1,71 @@
+package lectures
+
+import scala.None
+
+object AdvancedPatternMatching {
+  def main(args: Array[String]): Unit = {
+
+    val numbers = List(1)
+    numbers match {
+      case head :: Nil => println(s"the only element is $head")
+      case _ =>
+    }
+
+    // in a case where you can not create a case class but still need some pattern matching functions on the class
+    class Person(val name: String, val age: Int)
+
+    // "unapply" is trying to take an object and return its arguments while apply is a constructor which takes arguments and
+    // return an object.
+    object PersonPattern {
+      def unapply(person: Person): Option[(String,Int)] =
+        if (person.age < 21) None
+        else Some((person.name, person.age))
+
+      def unapply(age: Int): Option[String] =
+        Some(if (age< 21) "minor" else "major")
+    }
+
+
+    val bob = new Person("Bob", 25)
+    val greeting = bob match {
+      case PersonPattern(name, age) => s"Hi my name is $name and my age is $age"
+    }
+    println(greeting)
+
+    val legalStatus = bob.age match {
+      case PersonPattern(status) => println(s"my legal status is $status")
+    }
+
+
+
+    // exercise
+
+    val n = 45
+    val mathProperty = n match {
+      case x if (x > -10 && x < 10) => "single digit"
+      case x if (x % 2 == 0) => "even number"
+      case _ => "no property"
+    }
+    println(mathProperty)
+
+    //alternative way
+
+    object even {
+      def unapply(arg: Int): Boolean = arg % 2 == 0
+    }
+
+    object singleDigit {
+      def unapply(arg: Int): Boolean = arg > -10 && arg < 10
+    }
+
+    val newMathProperty = n match {
+      case singleDigit() => "single digit"
+      case even() => "even number"
+      case _ => "no property"
+    }
+    println(newMathProperty)
+
+
+  }
+
+}
